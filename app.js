31422f951b0c4d2c6eb81008fcb282e5b79f7ab6
piekario13 <jpiekarski@jpiekarski-nm.home>
2021-07-05 20:58:32 +0200

    week2

--- app.js
+++ app.js
@@ -1,51 +1,23 @@
-// ZADANIE 1
+// // ZADANIE 1
+// const a = prompt("Wprowadź pierwszą liczbę");
+// const b = prompt("Wprowadź drugą liczbę");
 
-// const name = prompt("Jak masz na imię");
-// const surname = prompt("Podaj nazwisko");
-// const age = prompt("Ile masz lat?");
-
-// if (age >= 18) {
-//   const fullName = (name + ' ' + surname);
-//   console.log(fullName);
-//   alert(fullName);
-// } else {
-//   console.log("Jesteś za młody");
-//   alert("Jesteś za młody");
+// const getResult = () => {
+//     console.log("Pierwsza liczba: " + a);
+//     console.log("Druga liczba: " + b);
+//     console.log("Wynik dodawania: " + (Number (a) + Number (b)));
+//     console.log("Wynik odejmowania: " + (Number (a) - Number (b)));
+//     console.log("Wynik mnozenia: " + (Number (a) * Number (b)));
+//     console.log(Number(a) || Number(b) === 0 ? "Nie dzielimy przez 0!!!!!" : "Wynik dzielenia: " + (Number (a) / Number (b)));
 // }
+
+// getResult();
 
 // ZADANIE 2
-
-// const a = prompt('Podaj pierwszą liczbę');
-// const b = prompt('Podaj drugą liczbę');
-// const operator = prompt('Jakie działanie chcesz wykonać? (+, -, /, *)');
-
-
-// if (operator === "+" ) {
-//   console.log(Number(a) + Number(b));
-// } else if (operator === "-" ) {
-//   console.log(Number(a) - Number(b));
-// } else if (operator === "*" ) {
-//   console.log(Number(a) * Number(b));
-// } else if (operator === "/" ) {
-//   console.log(Number(a) / Number(b))
-// } else console.log("Brak danych");
-
-// ZADANIE 3
-
-// const start = 1;
-// const count = 100;
-
-// for (let i = start; i <= count; i++) {
-//   if (i % 3 === 0 )
-//   console.log(i + " liczba podzielna przez trzy")
-// }
-
-// ZADANIE 4
-const words = prompt('Wpisz jakiś tekst');
-for (let i = 0; i < words.length; i++) {
-  if (i % 2 === 0 ) {
-    console.log(words[i].toUpperCase())
-  } else {
-    console.log(words[i])
-  }
-}
+// const setValue = prompt("Podaj liczbę");
+// const value = parseInt(setValue);
+// const checkNumber = () => value % 2 === 0 ? even() : odd()
+// const even = () => value
+// const odd = () => value * 2
+// checkNumber()
+// console.log(checkNumber())
